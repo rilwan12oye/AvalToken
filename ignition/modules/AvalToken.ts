79150f8e16e90c8fb1f3b19ec2be5b8f5929879a
Rilwan Oyewole <rilwan12.oyewole@gmail.com>
2024-08-07 07:32:54 +0000

    project done

---
+++ ignition/modules/AvalToken.ts
@@ -0,0 +1,17 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+
+const AvalModule = buildModule("AvalModule", (m) => {
+
+  const tokenName = m.getParameter("tokenName", "AvalToken");
+  const tokenSymbol = m.getParameter("tokenSymbol", "ATK");
+
+  const avalToken = m.contract("AvalToken", [tokenName, tokenSymbol]);
+
+  return { avalToken };
+});
+
+export default AvalModule;
+
+//to deploy
+//npx hardhat ignition deploy ignition/modules/Lock.ts --network localhost
